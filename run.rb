2ea9f2b1451970cb864f9ad02f68d05b3b248a37
gchkhvirkia <Georgechkhvirkia@gmail.com>
2014-05-13 22:28:45 +0300

    Update run.rb

--- run.rb
+++ run.rb
@@ -4,9 +4,7 @@ require './Controllers/PrinterController'
 
 require 'yaml'
 
-
 commandsCtrl = Controllers::CommandController.new 
-userCtrl = Controllers::UserController.new
 
 Controllers::PrinterController.wellcome
 
