a5e3278fe3ee4ac90d544ff6e00115758103d5ce
gchkhvirkia <Georgechkhvirkia@gmail.com>
2014-05-18 03:52:34 +0300

    Refactoring IOController

--- Controllers/IOController.rb
+++ Controllers/IOController.rb
@@ -4,6 +4,10 @@ module Controllers
 
   class IOController
 
+    USER_YAML_FILE = './users.yml'
+    SESSION_YAML_FILE = './session.yml'
+    TASK_YAML_FILE = './tasks.yml'
+
     def self.user_exists? user
 
       result = true
@@ -25,77 +29,34 @@ module Controllers
       result
     end
 
-    def self.generate_user_id users_array
-
-      new_user_id = rand(99999999) 
-
-      users_array.each do |item|
-        if new_user_id == item[:id]
-          new_user_id = generate_user_id (users_array)
-          break
-        end
-      end
- 
-      new_user_id
-    end
-
     def self.create_user user, pass
-
-      users_array = get_all_users  
-      
-      new_user_id = generate_user_id(users_array)
-
-      new_user_hash = {id: new_user_id, username: user, password: pass }
-
-      users_array.push(new_user_hash)
-
-      yaml_write('./users.yml', users_array)
-
-      # File.open('./users.yml', 'w') do |file|
-      #   file.puts users_array.to_yaml
-      # end
-
+      new_user_id = generate_user_id
+      new_user_hash = {id: new_user_id, username: user, password: pass } 
+      save_user new_user_hash
     end
 
     def self.create_session? user, pass
-      session_hash = {}
       result = false
-
       users_array = get_all_users  
-
       if users_array.any?
-
         users_array.each do |item|
           if user == item[:username] && pass == item[:password]
-            session_hash = {id: item[:id]}
+            yaml_write( SESSION_YAML_FILE, {id: item[:id]})
             result = true
             break
           end
         end
-
-        if result 
-
-          yaml_write('./session.yml', session_hash)
-
-          # File.open('./session.yml', 'w') do |session|
-          #   session.puts session_hash.to_yaml
-          # end
-        end 
       end
-
       result
     end
 
     def self.session_exists?
-      exists?('./session.yml')
+      exists?( SESSION_YAML_FILE )
     end
 
     def self.session_user_id
       if session_exists? 
-        session_info = []
-
-        session_info = yaml_load('./session.yml')
- 
+        session_info = get_session
         session_info[:id]
       else
         -1
@@ -103,12 +64,9 @@ module Controllers
     end
 
     def self.get_tasks
-      auth_user_id = session_user_id.to_i
- 
       user_tasks = []
-
+      auth_user_id = session_user_id.to_i
       all_tasks = get_all_tasks
-
       if all_tasks.any?
         all_tasks.each do |task|
           if task[:user_id] == auth_user_id
@@ -116,100 +74,99 @@ module Controllers
           end
         end
       end
-
       user_tasks
     end
 
     def self.new_task task_title, estimation
       auth_user_id = session_user_id
-
-      all_tasks = get_all_tasks
-
-      new_task_id = generate_task_id all_tasks
-
+      new_task_id = generate_task_id
       task_hash = { id: new_task_id, user_id: auth_user_id, title: task_title, due_date: estimation }
-
-      all_tasks.push(task_hash)
-
-      yaml_write('./tasks.yml', all_tasks)
-
-      # File.open('./tasks.yml', 'w') do |tasks|
-      #   tasks.puts all_tasks.to_yaml
-      # end
-      
+      save_task task_hash  
     end
 
-    def self.generate_task_id tasks_array
+    def self.save_element file, array, new_data
+      array.push(new_data)
+      yaml_write( file, array )
+    end
 
-      new_task_id = rand(99999999) 
+    def self.save_user new_user_hash
+      users_array = get_all_users 
+      save_element USER_YAML_FILE, users_array, new_user_hash
+    end
 
-      tasks_array.each do |item|
-        if new_task_id == item[:id]
-          new_task_id = generate_task_id (tasks_array)
+    def self.save_task new_task_hash 
+      all_tasks = get_all_tasks
+      save_element TASK_YAML_FILE, all_tasks, new_task_hash
+    end
+
+    def self.generate_task_id
+      generate_unique_id get_all_tasks 
+    end
+
+    def self.generate_user_id
+      generate_unique_id get_all_users  
+    end
+
+    def self.generate_unique_id data
+      new_id = rand(99999999) 
+
+      data.each do |item|
+        if new_id == item[:id]
+          new_id = generate_unique_id (data)
           break
         end
       end
- 
-      new_task_id
+      new_id
     end
 
     def self.delete_task task_id
-      all_tasks = []
-      index = 0
-      exists = false
-
       all_tasks = get_all_tasks
 
       if all_tasks.any?
-
+        index = 0
         all_tasks.each do |task|
           if task[:id] == task_id
-            exists = true
+            delete_task_from_file all_tasks, index
             break
           end
           index += 1
         end
-
-        all_tasks.slice!(index)
-
-        yaml_write('./tasks.yml', all_tasks)
-
-        # File.open('./tasks.yml', 'w') do |tasks|
-        #   tasks.puts all_tasks.to_yaml
-        # end
       end
     end
 
+    def self.delete_task_from_file task_array, index
+      task_array.slice!(index)
+      yaml_write( TASK_YAML_FILE, task_array)
+    end
+
     def self.destroy_session
-      File.delete('./session.yml')
+      File.delete( SESSION_YAML_FILE )
     end
 
     def self.exists? file_path
-       File.file?(file_path)
+      File.file?(file_path)
+    end
+
+    def self.get_session
+      yaml_load( SESSION_YAML_FILE )
+    end
+
+    def self.get_all_users
+      yaml_load( USER_YAML_FILE )
+    end
+
+    def self.get_all_tasks
+      yaml_load( TASK_YAML_FILE )
     end
 
     def self.yaml_load yaml_file_path 
       file_content = [] 
-      File.open( yaml_file_path, 'r') do |file|
-        file_content = YAML.load(file)
+      if exists? (yaml_file_path)
+        File.open( yaml_file_path, 'r') do |file|
+          file_content = YAML.load(file)
+        end
       end 
       file_content
-    end
-
-    def self.get_all_users
-      users_array = []
-      if exists?('./users.yml')
-        users_array = yaml_load('./users.yml')
-      end
-      users_array
-    end
-
-    def self.get_all_tasks
-      all_tasks = []
-      if exists?('./tasks.yml')
-        all_tasks = yaml_load('./tasks.yml')
-      end
-      all_tasks
     end
 
     def self.yaml_write yaml_file_path, data
